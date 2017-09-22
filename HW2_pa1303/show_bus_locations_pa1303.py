a8a9198620e23763fa836d807255f58886ded985
princeaker <abunkupa@mail.uc.edu>
2017-09-21 17:23:00 -0700

    finished part 1

--- HW2_pa1303/show_bus_locations_pa1303.py
+++ HW2_pa1303/show_bus_locations_pa1303.py
@@ -12,4 +12,16 @@ url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=" + key + \
 response = urllib2.urlopen(url)
 data = response.read().decode("utf-8")
 dataDict = json.loads(data)
-print (dataDict)
+
+access_point=dataDict['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
+access_point[0]['MonitoredVehicleJourney']['VehicleLocation']
+buses = []
+for i in range(len(access_point)):
+    name = access_point[i]['MonitoredVehicleJourney']['PublishedLineName']
+    location = access_point[i]['MonitoredVehicleJourney']['VehicleLocation']
+    buses.append((name, location))
+
+print('Bus Line : {}'.format(line))
+print("Number of active buses: %d" % len(buses))
+for i in range(len(buses)):
+    print("Bus {0} is at latitude {1} and longitude {2}".format(i, buses[i][1]['Latitude'], buses[i][1]['Longitude']))    
